6b3548ab1829ed6eaadc5eea8d33191ca34ce30e
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-04-24 19:03:44 -0500

    Added more verbs and their translations

--- Verregular App-Code/Home/Select/Model/IrregularVerbs.swift
+++ Verregular App-Code/Home/Select/Model/IrregularVerbs.swift
@@ -16,7 +16,19 @@ class IrregularVerbs {
             Verb(infinitive: "blow", pastSimple: "blew", participle: "blown"),
             Verb(infinitive: "draw", pastSimple: "drew", participle: "drawn"),
             Verb(infinitive: "eat", pastSimple: "ate", participle: "eaten"),
-            Verb(infinitive: "fall", pastSimple: "fell", participle: "fallen")
+            Verb(infinitive: "fall", pastSimple: "fell", participle: "fallen"),
+            Verb(infinitive: "be", pastSimple: "was(were)", participle: "been"),
+            Verb(infinitive: "begin", pastSimple: "began", participle: "begun"),
+            Verb(infinitive: "drink", pastSimple: "drank", participle: "drunk"),
+            Verb(infinitive: "feed", pastSimple: "fed", participle: "fed"),
+            Verb(infinitive: "leave", pastSimple: "left", participle: "left"),
+            Verb(infinitive: "meet", pastSimple: "met", participle: "met"),
+            Verb(infinitive: "ride", pastSimple: "rode", participle: "ridden"),
+            Verb(infinitive: "sell", pastSimple: "sold", participle: "sold"),
+            Verb(infinitive: "teach", pastSimple: "taught", participle: "taught"),
+            Verb(infinitive: "win", pastSimple: "won", participle: "won"),
+            Verb(infinitive: "break", pastSimple: "broke", participle: "broken"),
+            Verb(infinitive: "cut", pastSimple: "cut", participle: "cut")
         ]
     }
 }
